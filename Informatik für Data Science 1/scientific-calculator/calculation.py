d2ad69a32a8acd91a14ff8377c6b95a629e38684
Stas Cuprunov <stas.cuprunov@t-online.de>
2025-11-29 14:40:54 +0100

    implement calculate addition

--- Informatik für Data Science 1/scientific-calculator/calculation.py
+++ Informatik für Data Science 1/scientific-calculator/calculation.py
@@ -1,2 +1,56 @@
+from character_constants import *
+from text_constants import *
+
+# calculation interface
 def calculate(task: str) -> str:
-    return "Task = " + task
+    if len(task) == 0:
+        return ""
+    task = check_first_character(task)
+    task_list = []
+
+    store_element = ""
+    
+    for character in task:
+        if (character == CHARACTER_PLUS or character == CHARACTER_MINUS):
+            task_list.append(store_element)
+            task_list.append(character)
+            store_element = ""
+        else:
+            store_element += character
+
+    if (store_element != ""):
+        task_list.append(store_element)
+
+    if len(task_list) > 1:
+        if task_list[0] == CHARACTER_MINUS:
+            task_list[1] = CHARACTER_MINUS + task_list[1]
+            del task_list[0]
+            
+    result = 0
+    
+    for index, element in enumerate(task_list):
+        if index > len(task_list) - 2:
+            break
+        try:
+            if (index == 0):
+                result = float(element)
+                continue
+            if element == CHARACTER_PLUS:
+                result = result + float(task_list[index + 1])
+                index += 1
+            elif element == CHARACTER_MINUS:
+                result = result - float(task_list[index + 1])
+                index += 1
+        except:
+            return TEXT_INVALID_SYNTAX
+    if (result == 0):
+        return task
+    return str(result)
+
+def check_first_character(task):
+    first_character = task[0]
+    if (first_character == CHARACTER_DECIMAL_POINT):
+        return CHARACTER_ZERO + task
+    elif (first_character == CHARACTER_PLUS):
+        return task[1:]
+    return task
