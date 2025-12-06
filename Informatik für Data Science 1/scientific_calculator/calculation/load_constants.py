24969402f371096f27f16926808af296d64ff89f
Stas Cuprunov <stas.cuprunov@t-online.de>
2025-12-06 17:45:02 +0100

    implement constants

--- Informatik für Data Science 1/scientific_calculator/calculation/load_constants.py
+++ Informatik für Data Science 1/scientific_calculator/calculation/load_constants.py
@@ -2,7 +2,10 @@ import json
 import os
 
 def get_constants_for_calculation():
-    with open(os.path.join('constants', 'constants.json'), encoding="utf-8") as json_file:
+    with open(os.path.join("constants", "constants.json"), encoding="utf-8") as json_file:
         constants_for_calculation = json.load(json_file)
+        dictionary_for_constants = {}
 
-        return constants_for_calculation["constants"]
+        for element in constants_for_calculation["constants"]:
+            dictionary_for_constants[element["si-symbol"]] = element["value"]
+        return dictionary_for_constants
